c50c9cf610b8af4d9a1b9f02bc04d5a3d332dbc1
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-02-27 19:48:21 +0000

    fixed a compilation bug

--- src/source_location/source_location.cpp
+++ src/source_location/source_location.cpp
@@ -9,13 +9,13 @@ constexpr std::uint_least32_t source_location::column() const noexcept {
     return 0;
 }
 constexpr const char* source_location::function_name() const noexcept {
-    return "DUMMY source_location - please use newer (if possible) compiler."
+    return "DUMMY source_location - please use newer (if possible) compiler.";
 }
 constexpr const char* source_location::file_name() const noexcept {
-    return "DUMMY source_location - please use newer (if possible) compiler."
+    return "DUMMY source_location - please use newer (if possible) compiler.";
 }
 
-static source_location source_location::current() {
+source_location source_location::current() {
     return source_location{};
 }
 
