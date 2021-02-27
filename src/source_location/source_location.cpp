4a4367f0ae2bc9b17a618a0d8f09f48f99f8c0dc
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-02-27 19:59:53 +0000

    readded constexpr

--- src/source_location/source_location.cpp
+++ src/source_location/source_location.cpp
@@ -4,16 +4,19 @@
 
 #else
 
-std::uint_least32_t source_location::line() const noexcept {
+constexpr std::uint_least32_t source_location::line() const noexcept {
     return 0;
 }
-std::uint_least32_t source_location::column() const noexcept {
+
+constexpr std::uint_least32_t source_location::column() const noexcept {
     return 0;
 }
-const char* source_location::function_name() const noexcept {
+
+constexpr const char* source_location::function_name() const noexcept {
     return "DUMMY source_location - please use newer (if possible) compiler.";
 }
-const char* source_location::file_name() const noexcept {
+
+constexpr const char* source_location::file_name() const noexcept {
     return "DUMMY source_location - please use newer (if possible) compiler.";
 }
 
