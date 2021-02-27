6464f360af6a3ca65b1af8cc1a3766b1d222cf64
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-02-27 19:57:25 +0000

    fixed??

--- src/source_location/source_location.cpp
+++ src/source_location/source_location.cpp
@@ -1,17 +1,19 @@
 #include "source_location.hpp"
 
-#if not __has_include(<source_location>)
+#if __has_include(<source_location>)
 
-constexpr std::uint_least32_t source_location::line() const noexcept {
+#else
+
+std::uint_least32_t source_location::line() const noexcept {
     return 0;
 }
-constexpr std::uint_least32_t source_location::column() const noexcept {
+std::uint_least32_t source_location::column() const noexcept {
     return 0;
 }
-constexpr const char* source_location::function_name() const noexcept {
+const char* source_location::function_name() const noexcept {
     return "DUMMY source_location - please use newer (if possible) compiler.";
 }
-constexpr const char* source_location::file_name() const noexcept {
+const char* source_location::file_name() const noexcept {
     return "DUMMY source_location - please use newer (if possible) compiler.";
 }
 
