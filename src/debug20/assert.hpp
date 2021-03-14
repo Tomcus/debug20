d1a46f902d1dcd64797c7b959de91163ed5d1d3b
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-03-14 22:08:43 +0000

    rewrote assert api to use its recycle itself

--- src/debug20/assert.hpp
+++ src/debug20/assert.hpp
@@ -12,65 +12,53 @@ namespace d20 {
 	};
 
 
-	constexpr void assert_bool(const bool result, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
+	constexpr void assert_true(const bool result, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
 		if (!result) {
 			throw assertion_error(message, soc_loc);
 		}
 	}
 
+	constexpr void assert_false(const bool result, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
+		assert_true(!result, message, soc_loc);
+	}
+
 	template <typename type_a, typename type_b>
 	constexpr void assert_equals(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a != b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a == b, message, soc_loc);
 	}
 
 	template <typename type_a, typename type_b>
 	constexpr void assert_not_equals(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a == b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a != b, message, soc_loc);
 	}
 
 	template <typename type_a, typename type_b>
 	constexpr void assert_greater_then(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a <= b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a > b, message, soc_loc);
 	}
 
 	template <typename type_a, typename type_b>
 	constexpr void assert_lesser_then(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a >= b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a < b, message, soc_loc);
 	}
 
 	template <typename type_a, typename type_b>
 	constexpr void assert_greater_equal(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a < b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a >= b, message, soc_loc);
 	}
 	template <typename type_a, typename type_b>
 	constexpr void assert_lesser_equal(const type_a& a, const type_b& b, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (a > b) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(a <= b, message, soc_loc);
 	}
 
 	template<typename pointer>
 	constexpr void assert_null(const pointer p, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (p != nullptr) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(p == nullptr, message, soc_loc);
 	}
 
 	template<typename pointer>
 	constexpr void assert_not_null(const pointer p, const std::string_view& message = "", const source_location soc_loc = source_location::current()) {
-		if (p == nullptr) {
-			throw assertion_error(message, soc_loc);
-		}
+		assert_true(p != nullptr, message, soc_loc);
 	}
 }
 
