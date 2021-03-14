28e4fe86344f3b55b0ebda859f7043370ed40bd4
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-03-14 22:51:54 +0000

    added fail func to assert api

--- src/debug20/assert.cpp
+++ src/debug20/assert.cpp
@@ -4,7 +4,3 @@ using namespace d20;
 
 d20::assertion_error::assertion_error(const std::string_view message,
 									  const source_location& location): exception(message, location) { }
-
-const char* d20::assertion_error::what() const noexcept {
-	return error_message.c_str();
-}
